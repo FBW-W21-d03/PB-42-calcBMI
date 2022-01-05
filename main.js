c3aeddcb2ce16b90ad93c5dfbde2467e8f02ceeb
kawanaza <kawanbabani@gmail.com>
2022-01-05 09:30:32 +0100

    Done

---
+++ main.js
@@ -0,0 +1,9 @@
+/////JavaScript-calcBMI/////
+
+function calcBMI(gewicht, höhe) {
+  return gewicht / höhe / höhe;
+}
+//////Aufgabenstellung - Teil 1////
+let body = calcBMI(80, 1.77);
+
+console.log("BMI (John) ist :",body);

8500ac005b8bfbbe907b43f67859288a8ae1e54a
Enza <86748625+Enza-von-metaTronics@users.noreply.github.com>
2022-01-05 09:34:18 +0100

    PB42 - calcBMI

---
+++ index.js
@@ -0,0 +1,17 @@
+'use strict';
+
+console.log('JavaScript-calcBMI');
+
+// aufgabe teil1
+
+function bmi(kg , height){
+    return(kg / height / height)
+}
+
+let return1 = bmi(58 , 1.6);
+let return2 = bmi(120 , 1.8);
+
+// aufgabe teil2
+
+console.log(return1);
+console.log(return2);
